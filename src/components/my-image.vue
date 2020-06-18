c8073163380e314cdc07c69ed5cac29471f534e4
jinzi <468762932@qq.com>
2020-06-18 21:07:33 +0800

    完成素材管理，一部分发布文章

--- src/components/my-image.vue
+++ src/components/my-image.vue
@@ -1,33 +1,126 @@
 <template>
   <div class="container-my-image">
     <div class="img-btn" @click="openDialog">
-      <img src="../assets/default.png" alt />
+      <img :src="value || defaultImageUrl" alt />
     </div>
     <el-dialog :visible.sync="dialogVisible" width="720px">
       <el-tabs v-model="activeName" type="card">
-        <el-tab-pane label="素材库" name="list">素材件内容</el-tab-pane>
-        <el-tab-pane label="上传图片" name="image">上传图片内容</el-tab-pane>
+        <el-tab-pane label="素材库" name="list">
+          <el-radio-group @change="changeCollect" v-model="filterParams.collect" size="small">
+            <el-radio-button label="false">全部</el-radio-button>
+            <el-radio-button label="true">收藏</el-radio-button>
+          </el-radio-group>
+          <div class="img-list">
+            <div
+              @click="selectedImage(item.url)"
+              class="img-item"
+              v-for="item in images"
+              :key="item.id"
+              :class="{selected:item.url === selectedImageUrl}"
+            >
+              <img :src="item.url" alt />
+            </div>
+          </div>
+          <el-pagination
+            @current-change="changePage"
+            :page-size="filterParams.per_page"
+            :current-page="filterParams.page"
+            background
+            layout="prev, pager, next"
+            :total="total"
+            hide-on-single-page
+          ></el-pagination>
+        </el-tab-pane>
+        <el-tab-pane label="上传图片" name="image">
+          <el-upload
+            class="avatar-uploader"
+            action="http://ttapi.research.itcast.cn/mp/v1_0/user/images"
+            name="image"
+            :headers="headers"
+            :show-file-list="false"
+            :on-success="uploadImageSuccess"
+          >
+            <img v-if="updateImageUrl" :src="updateImageUrl" class="avatar" />
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+        </el-tab-pane>
       </el-tabs>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        <el-button type="primary" @click="confirmImage">确 定</el-button>
       </span>
     </el-dialog>
   </div>
 </template>
 
 <script>
+import auth from "@/utils/auth.js";
+import defaultImageUrl from "@/assets/default.png";
 export default {
   name: "my-image",
+  props: ["value"],
   data() {
     return {
       dialogVisible: false,
-      activeName: "list"
+      activeName: "list",
+      filterParams: {
+        page: 1,
+        per_page: 8,
+        collect: false
+      },
+      images: [],
+      total: 0,
+      selectedImageUrl: null,
+      headers: { Authorization: `Bearer ${auth.getUser().token}` },
+      updateImageUrl: null,
+      defaultImageUrl
     };
   },
   methods: {
+    confirmImage() {
+      if (this.activeName === "list") {
+        if (!this.selectedImageUrl) return this.$message.warning("请选择图片");
+        this.$emit("input", this.selectedImageUrl);
+
+        this.dialogVisible = false;
+      }
+      if (this.activeName === "image") {
+        if (!this.updateImageUrl) return this.$message.warning("请上传图片");
+        this.$emit("input", this.updateImageUrl);
+
+        this.dialogVisible = false;
+      }
+    },
+    uploadImageSuccess(res) {
+      this.$message.success("上传成功");
+      this.updateImageUrl = res.data.url;
+    },
+    selectedImage(url) {
+      this.selectedImageUrl = url;
+    },
     openDialog() {
       this.dialogVisible = true;
+      this.activeName = "list";
+      this.filterParams.page = 1;
+      this.filterParams.collect = false;
+      this.updateImageUrl = null;
+      this.selectedImageUrl = null;
+      this.getImages();
+    },
+    async getImages() {
+      const {
+        data: { data }
+      } = await this.$http.get("/user/images", { params: this.filterParams });
+      this.images = data.results;
+      this.total = data.total_count;
+    },
+    changePage(newPage) {
+      this.filterParams.page = newPage;
+      this.getImages();
+    },
+    changeCollect() {
+      this.filterParams.page = 1;
+      this.getImages();
     }
   }
 };
@@ -48,5 +141,38 @@ export default {
       display: block;
     }
   }
+}
+
+.img-list {
+  .img-item {
+    width: 150px;
+    height: 120px;
+    border: 1px dashed #ddd;
+    display: inline-block;
+    margin-top: 10px;
+    margin-right: 15px;
+
+    &.selected {
+      position: relative;
+      &::after {
+        content: "";
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.4) url(../assets/selected.png) no-repeat
+          center / 50px;
+      }
+    }
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+
+/deep/ .el-dialog__body {
+  padding-bottom: 0;
 }
 </style>
--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -2,34 +2,38 @@
   <div class="container-publish">
     <el-card>
       <div slot="header">
-        <my-bread>发布文章</my-bread>
+        <my-bread>{{$route.query.id?'修改文章':"发布文章"}}</my-bread>
       </div>
-      <el-form label-width="120px">
-        <el-form-item label="标题：" v-model="articleForm.title">
-          <el-input placeholder="请输入文章标题" style="width:400px;"></el-input>
+      <el-form label-width="120px" ref="articleForm" :model="articleForm" :rules="articleRules">
+        <el-form-item label="标题：" v-model="articleForm.title" prop="title">
+          <el-input v-model="articleForm.title" placeholder="请输入文章标题" style="width:400px;"></el-input>
         </el-form-item>
-        <el-form-item label="内容：">
-          <quill-editor v-model="articleForm.content" :options="editorOption"></quill-editor>
+        <el-form-item label="内容：" prop="content">
+          <quill-editor @blur="checkContent" v-model="articleForm.content" :options="editorOption"></quill-editor>
         </el-form-item>
-        <el-form-item label="封面：">
-          <el-radio-group v-model="articleForm.cover.type">
+        <el-form-item label="封面：" prop="cover">
+          <el-radio-group @change="articleForm.cover.images=[]" v-model="articleForm.cover.type">
             <el-radio :label="1">单图</el-radio>
             <el-radio :label="3">三图</el-radio>
             <el-radio :label="0">无图</el-radio>
             <el-radio :label="-1">自动</el-radio>
           </el-radio-group>
-          <div style="height:160px;">
-            <my-image></my-image>
-            <my-image></my-image>
-            <my-image></my-image>
+          <div style="height:160px;" v-if="articleForm.cover.type=== 1">
+            <my-image v-model="articleForm.cover.images[0]"></my-image>
+          </div>
+          <div style="height:160px;" v-if="articleForm.cover.type=== 3">
+            <my-image v-for="i in 3" :key="i" v-model="articleForm.cover.images[i-1]"></my-image>
           </div>
         </el-form-item>
-        <el-form-item label="频道：">
+        <el-form-item label="频道：" prop="channel_id">
           <my-channel v-model=" articleForm.channel_id"></my-channel>
         </el-form-item>
-        <el-form-item>
-          <el-button type="primary">发布文章</el-button>
-          <el-button>存为草稿</el-button>
+        <el-form-item v-if="$route.query.id">
+          <el-button type="success" @click="addArticle(false)">修改文章</el-button>
+        </el-form-item>
+        <el-form-item v-else>
+          <el-button type="primary" @click="addArticle(false)">发布文章</el-button>
+          <el-button @click="addArticle(true)">存为草稿</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -48,14 +52,45 @@ export default {
   name: "publish-page",
   components: { quillEditor },
   data() {
+    const checkCover = (rule, value, callback) => {
+      if (value.type === 1) {
+        if (!value.images[0]) {
+          return callback(new Error("请选择一张封面图"));
+        }
+      }
+      if (value.type === 3) {
+        if (!value.images[0] || !value.images[1] || !value.images[2]) {
+          return callback(new Error("请选择三张封面图"));
+        }
+      }
+      callback();
+    };
     return {
+      articleRules: {
+        title: [
+          { required: true, message: "请输入文章标题", trigger: "blur" },
+          {
+            min: 5,
+            max: 30,
+            message: "请输入5-30字符串的文章标题",
+            trigger: "blur"
+          }
+        ],
+        content: [
+          { required: true, message: "请输入文章内容", trigger: "blur" }
+        ],
+        cover: [{ validator: checkCover, trigger: "blur" }],
+        channel_id: [
+          { required: true, message: "请选择所属频道", trigger: "change" }
+        ]
+      },
       // 文章表单数据对象
       articleForm: {
         title: null,
         content: null,
         cover: {
           type: 1,
-          images: null
+          images: []
         },
         channel_id: null
       },
@@ -73,6 +108,59 @@ export default {
         }
       }
     };
+  },
+  watch: {
+    "articleForm.cover": {
+      handler() {
+        this.$refs.articleForm.validateField("cover");
+      },
+      deep: true
+    },
+    "$route.query.id": function() {
+      if (this.$route.query.id) {
+        // 填充表单
+        this.getArticle();
+      } else {
+        this.articleForm = {
+          title: null,
+          content: null,
+          cover: {
+            type: 1,
+            images: []
+          },
+          channel_id: null
+        };
+      }
+    }
+  },
+  created() {
+    if (this.$route.query.id) {
+      this.getArticle();
+    }
+  },
+  methods: {
+    async getArticle() {
+      const {
+        data: { data }
+      } = await this.$http.get("/articles/" + this.$route.query.id);
+      this.articleForm = data;
+    },
+    addArticle(draft) {
+      this.$refs.articleForm.validate(async valid => {
+        if (valid) {
+          try {
+            await this.$http.post(`/articles?draft=${draft}`, this.articleForm);
+            this.$message.success(draft ? "存入草稿成功" : "添加发布成功");
+            this.$router.push("/article");
+          } catch (e) {
+            this.$message.error(draft ? "存入草稿失败" : "添加发布失败");
+          }
+        }
+      });
+    },
+    checkContent() {
+      this.$refs.articleForm.validateField("content");
+    }
   }
 };
 </script>
